df5623c656b141525b8a6bd5d30e23be36f0d0dd
Michel Tossaint <mmmtossaint@gmail.com>
2015-10-21 21:30:50 +0200

    Another update

--- collect.py
+++ collect.py
@@ -1,36 +1,25 @@
-import os
-import time
 
-# At start do until next hour
-mintogo = 60-int(time.strftime("%M"))-3
-now = time.strftime("_%Y%m%d_%H")
-command = "rtl_power -f 1415M:1425M:5k -i 10 -g 50 -c 0.40 -e "+str(mintogo)+"m data"+now+".csv"
-print (time.strftime("%Y-%m-%d %H:%M:%S ") + command)
-os.system(command)
-command = "python heatmap.py --ytick 60s data"+now+".csv power"+now+".png"
-print (time.strftime("%Y-%m-%d %H:%M:%S ") + command)
-os.system(command)
-os.system("mv power*.png ../Share/")
-os.system("mv max*.png ../Share/")
-
-# Repeat every hour
-while True:
-
-    # wait untill next hour
-    print (time.strftime("%Y-%m-%d %H:%M:%S ") + "Wait untill next hour")
-    while True:
-        if int(time.strftime("%M"))==0:
-            break
-        time.sleep(1)
-
-    now = time.strftime("_%Y%m%d_%H")
-    command = "rtl_power -f 1415M:1425M:5k -i 10 -g 50 -c 0.40 -e 58m data"+now+".csv"
-    print (time.strftime("%Y-%m-%d %H:%M:%S ") + command)
-    os.system(command)
-    command = "python heatmap.py --ytick 60s data"+now+".csv power"+now+".png"
-    print (time.strftime("%Y-%m-%d %H:%M:%S ") + command)
-    os.system(command)
-    os.system("mv power*.png ../Share/")
-    os.system("mv max*.png ../Share/")
+if __name__ == '__main__':
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    tb = top_block()
     
+    UpdateRate = 5*60 # In seconds
+    DataDir = '/media/michel/SETI'
+    # Repeat forever
+    while True:
 
+        # start and wait untill next period
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(now + ' Started new file')
+        tb.start()
+        while True:
+            NumSeconds = int(time.strftime("%M"))*60+int(time.strftime("%S"))
+            if NumSeconds % UpdateRate ==0:
+                now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                tb.set_prefix("/media/michel/SETI/" + datetime.now().strftime("%Y/%Y%m%d"))
+                time.sleep(1)
+                break
+            time.sleep(1)
+        tb.stop()
+        tb.wait()
